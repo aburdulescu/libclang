a4c6b636feec0066acaeb0a3a1f3445ec253c960
Andrei Burdulescu <andrei.burdulescu@pm.me>
2023-01-20 21:02:14 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-LIBCLANG_ROOT=$(cd $(dirname $0); pwd)
+LIBCLANG_ROOT=$(cd -P $(dirname $0); pwd)
 
-export LD_LIBRARY_PATH=$LIBCLANG_ROOT/libclang/lib
+export LD_LIBRARY_PATH=$LIBCLANG_ROOT/lib
 
 echo "LIBCLANG_ROOT=$LIBCLANG_ROOT"
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
